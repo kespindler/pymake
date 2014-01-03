7f34fd3c8ba6787f056a8b4eb09d210844f856be
Kurt <kespindler@gmail.com>
2014-01-02 21:47:42 -0800

    initial commit

---
+++ Pymake.py
@@ -0,0 +1,24 @@
+from pyrake import *
+
+@ignore
+def helper():
+    print 'helping'
+
+def all():
+    """All the shit. """
+    print 'all'
+    build()
+
+def clean():
+    """Clean shit. """
+    print 'cleaning'
+
+def build(depends=clean):
+    """Build shit. """
+    print 'building'
+
+def withargs(arg1, arg2, depends=clean):
+    """Demo with args. """
+    print 'withargs'
+    print arg1
+    print arg2
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pymake',
+      version='0.1',
+      description='Python make',
+      url='http://github.com/kespindler/pymake',
+      author='Python Make',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
